507c4389191d83d81357af3305a0ec2e2647b0ac
dalip03 <dalipdivakar@gmail.com>
2024-06-04 10:56:10 +0530

    new changes

--- basics/script.js
+++ basics/script.js
@@ -129,3 +129,28 @@ gsap.to("#page3 h1",{
         pin:true
     }
 })
+
+// ..............................tl......
+
+var path =`M 10 100 Q 250 100 1250 100`
+var finalPath =`M 10 100 Q 250 100 1250 100`
+
+var string = document.querySelector("#string")
+
+string.addEventListener("mousemove" , function (dets) {
+    path=`M 10 100 Q ${dets.x} ${dets.y} 1250 100`
+
+    gsap.to("svg path" , {
+        attr:{d:path},
+        duration:0.3,
+        ease:"power3.out"
+    })
+})
+
+string.addEventListener("mouseleave" , function(){
+    gsap.to("svg path",{
+       attr:{d:finalPath},
+       duration:1.5,
+       ease:"elastic.out(1,0.2)"
+    })
+})
