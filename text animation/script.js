360297de2df914979502eb2557b953fcda2d809c
dalip03 <dalipdivakar@gmail.com>
2024-06-05 13:01:57 +0530

    text animation

---
+++ text animation/script.js
@@ -0,0 +1,31 @@
+var main =document.querySelector("#main")
+var cursor =document.querySelector("#cursor")
+var tl = gsap.timeline()
+
+main.addEventListener("mousemove" , function(dets){
+    gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y,
+        duration:1
+    })
+})
+
+tl.from("#nav h1", {
+    y:-100,
+    duration:1,
+    delay:0.5,
+    opacity:0
+})
+
+tl.from("#menu h4", {
+     y:-50,
+    opacity:0,
+    duration:0.4,
+    stagger:2
+})
+tl.from("#page1 h1",{
+    x:50,
+    duration:1,
+    opacity:0,
+    scale:0
+})
